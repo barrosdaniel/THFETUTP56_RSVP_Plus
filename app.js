626f25220ef34db8699e38d2fdc55a3f11ac50a3
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-07-28 22:16:22 +0800

    feature: Add form event handler to add card with name entered in the form input field

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  console.log(input.value);
+  const text = input.value;
+  const ul = document.getElementById('invitedList');
+  const li = document.createElement('li');
+  li.textContent = text;
+  ul.appendChild(li);
   input.value = '';
 });
