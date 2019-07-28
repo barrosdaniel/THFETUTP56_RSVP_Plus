7559536698490a7ceb4ccf6ab607e808cb6c3628
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-07-28 22:07:55 +0800

    chore: Copy starter files in

---
+++ app.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  console.log(input.value);
+  input.value = '';
+});
